a181a07cd11376adbfb04a817f4bb30fd00f82ff
Nathan Ngo <nathanngo@Nathans-MacBook-Pro.local>
2022-05-17 22:43:45 +0700

    lab6

---
+++ lab-sessions/lab-6/ex_2_template_matching.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+def main():
+    img1 = cv2.imread('src-imgs/jisoo_1.jpeg',cv2.IMREAD_GRAYSCALE)
+    #queryImage
+    img2 = cv2.imread('src-imgs/jisoo_2.jpeg',cv2.IMREAD_GRAYSCALE) 
+    # Initiate SIFT detector
+    sift = cv2.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+    # BFMatcher with default params
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2,k=2)
+    # Apply ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append([m])
+    # cv.drawMatchesKnn expects list of lists as matches.
+    img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    cv2.imshow('image3', img3)
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
